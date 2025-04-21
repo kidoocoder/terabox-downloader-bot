37678065f3b65bc4980c426d172ff07217bc3b15
vicky0604hello <choudharyji0604e@gmail.com>
2025-04-21 23:51:22 +0530

    take some harmful steps to prevent the user from

--- config.py
+++ config.py
@@ -5,19 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Pyrogram credentials
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID", 26169983))
+API_HASH = os.environ.get("API_HASH", "c69f585ca5aee54e14c04cb02607d679")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7700343108:AAG6E7jCaY3ERdkKb1goy8a21hiIZYrLEXo")
 
 # Bot owner ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
+OWNER_ID = int(os.environ.get("OWNER_ID", 7767099543))
 
 # MongoDB connection URI
-MONGO_URI = os.environ.get("MONGO_URI")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://KingOfHell:Highspeedorg@cluster0.ha2cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Force subscribe channels
-CHANNEL_1 = os.environ.get("CHANNEL_1")  # Channel username without '@' or channel ID
-CHANNEL_2 = os.environ.get("CHANNEL_2")  # Channel username without '@' or channel ID
+CHANNEL_1 = os.environ.get("CHANNEL_1", "Guppppp_Shuppppp")
+CHANNEL_2 = os.environ.get("CHANNEL_2", "SYNTAX_WORLD")  # Channel username without '@' or channel ID
+
 
 # Bot settings
-MAX_FREE_DOWNLOADS = 3
+MAX_FREE_DOWNLOADS = 50
